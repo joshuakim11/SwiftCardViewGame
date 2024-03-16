34f968f1e24cd9f300b292bdfb98a41fc3abdc56
Joshua Kim <joshuakim11@gmail.com>
2024-03-16 16:11:14 -0500

    Initial Commit

---
+++ Memorize Game/Memorize_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_GameApp.swift
+//  Memorize Game
+//
+//  Created by Joshua Kim on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
